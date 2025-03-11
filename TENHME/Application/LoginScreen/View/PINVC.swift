d13aa3b2ee74de7f83dcfd2aee76e55b649d923a
Phanna <sophannasam25@gmail.com>
2025-03-11 18:07:58 +0900

    - new added

---
+++ TENHME/Application/LoginScreen/View/PINVC.swift
@@ -0,0 +1,29 @@
+//
+//  PINVC.swift
+//  TENHME
+//
+//  Created by Sam Sophanna on 3/11/25.
+//
+
+import UIKit
+
+class PINVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TENHME/Application/LoginScreen/View/PhoneNumberInputVC.swift
@@ -0,0 +1,29 @@
+//
+//  PhoneNumberInputVC.swift
+//  TENHME
+//
+//  Created by Sam Sophanna on 3/11/25.
+//
+
+import UIKit
+
+class PhoneNumberInputVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
